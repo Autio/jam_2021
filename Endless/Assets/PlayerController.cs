26a97f399d321c68481bf4c5928c99cc86d658a9
Maurizio Scuiar <vorrin.tal@gmail.com>
2021-03-05 21:52:42 +0100

    -fucking shit fuck, little guy walks around, I'm a bona fide genius

---
+++ Endless/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // private InputAction moveAction;
+    // private PlayerInput playerInput;
+
+    private EndlessInputActions inputActions;
+
+    void Awake()
+    {
+        inputActions = new EndlessInputActions();
+        inputActions.Player.Enable();
+    }
+
+    void Update()
+    {
+        var moveVec = inputActions.Player.Move.ReadValue<Vector2>();
+        Debug.Log(moveVec);
+        GetComponent<NavMeshAgent>().Move(new Vector3(moveVec.x,0,moveVec.y) * Time.deltaTime);
+    }
+}
