682d4ed62b2c02fb82344dab9cd9c836ac2b402d
Petri Autio <petri.mikael.autio@gmail.com>
2021-03-06 03:22:05 +0200

    Jumpy model

---
+++ Endless/Assets/Scripts/EnemyModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.layer);
+        if(collision.gameObject.layer == 8)
+        {
+            transform.parent.GetComponent<Enemy>().SetJumping(false);
+
+        }
+    }
+
+}
