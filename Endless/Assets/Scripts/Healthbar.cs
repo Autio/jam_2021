4f30b31d64c75a80f845e7ee111008f093a1c9a0
Petri Autio <petri.mikael.autio@gmail.com>
2021-03-06 20:30:44 +0200

    wip

--- Endless/Assets/Scripts/Healthbar.cs
+++ Endless/Assets/Scripts/Healthbar.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Healthbar : MonoBehaviour
 {
-    [SerializeField]
-    private int maxHealth = 100;
-    private int currentHealth;
 
-    public event Action<float> OnHealthPctChanged = delegate {};
-    // Start is called before the first frame update
+    [SerializeField]
+    private Image foregroundImage;
+    [SerializeField] 
+    private float updateSpeedSecongs = 0.5f;
+
+    private void Awake()
+    {
+        GetComponentInParent<EnemyBase>().OnHealthPctChanged += HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(float pct)
+    {
+        StartCoroutine(ChangeToPct(pct));
+    }
+
+    private IEnumerator  ChangeToPct(float pct){
+        float preChangePct = foregroundImage.fillAmount;
+        float elapsed = 0f;
+
+        while (elapsed <updateSpeedSeconds)
+        {
+            elapsed += Time.deltaDime;
+        }
+    }
+
+
+    // [SerializeField]
+    // private int maxHealth = 100;
+    // private int currentHealth;
+
+    // public event Action<float> OnHealthPctChanged = delegate {};
+    // // Start is called before the first frame update
+    
+    // void OnEnable()
+    // {
+    //     currentHealth = maxHealth;
+    // }
+
+    // public void ModifyHealth(int amount)
+    // {
+    //     currentHealth += amount;
+    //     float currentHealthPct = (float)currentHealth / (float)maxHealth; 
+    //     OnHealthPctChanged(currentHealthPct);
+    // }
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
